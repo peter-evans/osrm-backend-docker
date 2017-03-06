81ff556f3e309ff073fbef5a6a4793a04690780b
peter-evans <peter-evans@users.noreply.github.com>
2017-03-06 17:27:35 +0900

    Updated travis config

--- tests/wait-for-http.sh
+++ tests/wait-for-http.sh
@@ -3,16 +3,16 @@
 WFH_TIMEOUT=600
 WFH_ELAPSED=0
 
-echo "Waiting for a response from http://$1:$2"
+echo "Waiting for a response from $1"
 
-until $(curl --output /dev/null --silent --head --fail http://$1:$2); do
+until $(curl --output /dev/null --silent --head --fail $1); do
     sleep 5
     WFH_ELAPSED=$((WFH_ELAPSED+5))
     if [ "$WFH_ELAPSED" -gt "$WFH_TIMEOUT" ]; then
-    	echo "Timed out waiting for a response from http://$1:$2"
+    	echo "Timed out waiting for a response from $1"
     	exit 1
     fi
 done
 
-echo "Received response from http://$1:$2"
+echo "Received response from $1"
 exit 0
